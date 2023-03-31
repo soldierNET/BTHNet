2a78da474a3e59da26ea9e6df044ca97718ce6a9
Admin <Admin@LAB2-MAY16>
2023-03-31 15:00:26 +0700

    bai thuc hanh 10

---
+++ BaiThucHanh10/Controller/UserController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BaiThucHanh10.Controller
+{
+    public class UserController
+    {
+        public static string connectionString
+        {
+            get => ConfigurationManager.ConnectionStrings["UserDB"].ConnectionString;
+        }
+
+        public static bool Exist(string username)
+        {
+            string queryString = @"SELECT COUNT(*) FROM [dbo].[UserInfo] WHERE Username = @name";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@name", username);
+            connection.Open();
+            int count = (int)command.ExecuteScalar();
+            connection.Close();
+
+            if (count > 0)
+                return true;
+
+            return false;
+        }
+        
+        public static User GetUser(string username)
+        {
+            string queryString = @"SELECT * FROM [dbo].[UserInfo] WHERE Username = @name";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@name", username);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                return new User(reader["FirstName"].ToString(),
+                    reader["LastName"].ToString(),
+                    reader["Email"].ToString(),
+                    (bool)reader["Gender"],
+                    reader["Address"].ToString(),
+                    reader["Username"].ToString(),
+                    reader["Password"].ToString(),
+                    reader["Avatar"].ToString()
+                    );
+            }
+            connection.Close();
+            return null;
+        }
+
+        public static DataTable GetDataTable()
+        {
+            string queryString = @"SELECT * FROM [dbo].[UserInfo]";
+            SqlDataAdapter adapter = new SqlDataAdapter(queryString, connectionString);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        public static int Count()
+        {
+            string queryString = @"SELECT COUNT(*) FROM [dbo].[UserInfo]";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+            int count = (int)command.ExecuteScalar();
+            connection.Close();
+            return count;
+        }
+
+
+        public static bool Insert(User user)
+        {
+            string queryString = @"INSERT INTO [dbo].[UserInfo] 
+                                    ([Id], [Username], [Password], [FirstName], [LastName], [Email], [Gender], [Address], [Avatar])
+                                    VALUES (@id, @username, @password, @firstName, @lastName, @email, @gender, @address, @avatar)";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@id", Count()+1);
+            command.Parameters.AddWithValue("@username", user.username);
+            command.Parameters.AddWithValue("@password", user.password);
+            command.Parameters.AddWithValue("@firstName", user.firstName);
+            command.Parameters.AddWithValue("@lastName", user.lastName);
+            command.Parameters.AddWithValue("@gender", user.gender);
+            command.Parameters.AddWithValue("@email", user.email);
+            command.Parameters.AddWithValue("@address", user.address);
+            command.Parameters.AddWithValue("@avatar", user.avatar);
+
+            connection.Open();
+            int inserted = (int)command.ExecuteNonQuery();
+            connection.Close();
+
+            return (inserted > 0);
+        }
+
+        public static bool Update(User user)
+        {
+            string queryString = @"UPDATE [dbo].[UserInfo]
+                                    SET Password = @password, 
+                                        FirstName = @firstName, 
+                                        LastName = @lastName, 
+                                        Email = @email, 
+                                        Gender = @gender, 
+                                        Address = @address,
+                                        Avatar = avatar
+                                    WHERE Username = @username";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@username", user.username);
+            command.Parameters.AddWithValue("@password", user.password);
+            command.Parameters.AddWithValue("@firstName", user.firstName);
+            command.Parameters.AddWithValue("@lastName", user.lastName);
+            command.Parameters.AddWithValue("@gender", user.gender);
+            command.Parameters.AddWithValue("@email", user.email);
+            command.Parameters.AddWithValue("@address", user.address);
+            command.Parameters.AddWithValue("@avatar", user.avatar);
+            connection.Open();
+            int updated = (int)command.ExecuteNonQuery();
+            connection.Close();
+
+            return (updated > 0);
+        }
+        
+        public static bool Delete(string username)
+        {
+            string queryString = @"DELETE FROM [dbo].[UserInfo] 
+                                    WHERE Username = @username";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@username", username);
+
+            connection.Open();
+            int deleted = (int)command.ExecuteNonQuery();
+            connection.Close();
+            return (deleted > 0);
+        }
+    }
+}
---
+++ BaiThucHanh10/quanLyTaiKhoan.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QuanLyTaiKhoan.aspx.cs" Inherits="BaiThucHanh10.quanLyTaiKhoan" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="QuanLyTaiKhoan.css" />
+    <title>Quản lý tài khoản</title>
+</head>
+<body>
+    <form id="form" runat="server">
+        <h1>Quản lý tài khoản</h1>
+        <div class="form-container">
+            <label for="txtUsername">Tên đăng nhập</label>
+            <input type="text" id="txtUsername" runat="server" />
+            <label for="txtPassword">Mật khẩu</label>
+            <asp:TextBox type="password" ID="txtPassword" runat="server"></asp:TextBox>
+            <label for="txtLastName">Họ và tên đệm</label>
+            <input type="text" name="" id="txtLastName" runat="server" />
+            <label for="txtFirstName">Tên</label>
+            <input type="text" name="" id="txtFirstName" runat="server" />
+            <label for="txtEmail">Email</label>
+            <input type="text" name="" id="txtEmail" runat="server" />
+            <label for="">Giới tính</label>
+            <div class="input-group">
+                <label for="radMale">
+                    <input type="radio" name="gender" id="radMale" runat="server" value="male"/>Nam</label>
+                <label for="radFemale">
+                    <input type="radio" name="gender" id="radFemale" runat="server" value="female"/>Nữ</label>
+            </div>
+            <label for="txtAddress">Địa chỉ</label>
+            <input type="text" name="" id="txtAddress" runat="server" />
+            <label for="txtAvatar">Hình ảnh</label>
+            <input type="file" name="" id="txtAvatar" runat="server" />
+            <div class="input-group">
+                <button type="submit" runat="server" id="btnSave" onserverclick="btnSave_ServerClick">Lưu</button>
+                <button type="submit" runat="server" id="btnDelete" onserverclick="btnDelete_ServerClick">Xoá</button>
+                <button type="reset" runat="server" id="btnReset" onserverclick="btnReset_ServerClick">Xoá trống form</button>
+            </div>
+            <div class="input-group">
+                <p id="txtMessage" runat="server"></p>
+            </div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="ID" ReadOnly="True" />
+                <asp:BoundField DataField="Username" HeaderText="Username" ReadOnly="True" />
+                <asp:BoundField DataField="Password" HeaderText="Password" ReadOnly="True" />
+                <asp:BoundField DataField="FirstName" HeaderText="First name" ReadOnly="True" />
+                <asp:BoundField DataField="LastName" HeaderText="Last name" ReadOnly="True" />
+                <asp:BoundField DataField="Gender" HeaderText="gender" ReadOnly="True" />
+                <asp:BoundField DataField="Email" HeaderText="Email" ReadOnly="True" />
+                <asp:BoundField DataField="Address" HeaderText="Address" ReadOnly="True" />
+                <asp:ImageField DataImageUrlField="Avatar" DataImageUrlFormatString="~/images/{0}" HeaderText="Avatar">
+                    <ControlStyle Height="100px" Width="100px" />
+                </asp:ImageField>
+                <asp:CommandField ButtonType="Button" ShowSelectButton="True" />
+                <asp:HyperLinkField DataNavigateUrlFields="Username" DataNavigateUrlFormatString="ChinhSuaTaiKhoan.aspx?username={0}" Text="Edit">
+                <ControlStyle CssClass="btn" />
+                </asp:HyperLinkField>
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
